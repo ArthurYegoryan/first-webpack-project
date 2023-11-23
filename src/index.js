b2ab3fb96bc1837558f58d98a8e1102d7c548b3d
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2023-11-23 19:56:02 +0400

    feat(webpack,config.js): add css and file loaders, download plugins and loaders, add css folder for style files and img folder for images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import styles from "./style.css"
+import "../index.html";
+import "../css/style.css";
+import img from "../img/hacker.jpg";
 
-console.log("Barlus");
-console.log("Hayer");
+const root = document.querySelector("#root");
+const body = document.querySelector("body");
+
+const textArea = document.createElement("p");
+textArea.setAttribute("id", "text-area");
+textArea.textContent = "Barlus Hayer!";
+root.appendChild(textArea);
+
+const picture = document.createElement("img");
+picture.setAttribute("src", "./img/hacker.jpg");
+picture.setAttribute("alt", "picture");
+body.appendChild(picture);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
 
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
+    mode: "production",  // "production", "development", "none"
     entry: {
         main: path.resolve(__dirname, "src/index.js")
     },
@@ -15,13 +17,29 @@ module.exports = {
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, "index.html")
         }),
+        new MiniCssExtractPlugin({
+            filename: "[hash].css"
+        }),
         new CleanWebpackPlugin()
     ],
     module: {
         rules: [
             {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                // use: ["style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
             },
         ],
     },
