ab1312d39a690cae792684492b75f9e09a870b2e
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2023-11-23 20:55:05 +0400

    add configs for image, but still don't work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "../index.html";
 import "../css/style.css";
-import img from "../img/hacker.jpg";
+import hackerImage from "../img/hacker.jpg";
 
 const root = document.querySelector("#root");
 const body = document.querySelector("body");
@@ -11,6 +11,7 @@ textArea.textContent = "Barlus Hayer!";
 root.appendChild(textArea);
 
 const picture = document.createElement("img");
-picture.setAttribute("src", "./img/hacker.jpg");
-picture.setAttribute("alt", "picture");
+// picture.setAttribute("src", img);
+picture.src = hackerImage;
+picture.setAttribute("alt", "hackerImage");
 body.appendChild(picture);
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,9 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader, "css-loader"]
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
+                // test: /\.(png|jpe?g|gif)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
                 use: [
                     {
                         loader: 'file-loader',
